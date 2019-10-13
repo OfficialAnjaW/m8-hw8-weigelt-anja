64d93e8563e98e598b4561d9c5b25774beda5943
Anja Weigelt <aweigelt@ufl.edu>
2019-10-13 11:16:26 -0400

    Create JS hover effect for first button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // It is always helpful to use comments in your code!
+
+// Menu One
+
+var buttonOne = document.querySelector('.button-one');
+var menuOne = document.querySelector('.hidden-one');
+
+function toggleMenuOne() {
+  menuOne.classList.toggle('show-menu-one');
+}
+
+buttonOne.addEventListener('mouseover', toggleMenuOne);
+buttonOne.addEventListener('mouseleave', toggleMenuOne);
